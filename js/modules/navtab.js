d668c4b25916156d1efa4bd4cfd53f129f2c093c
Eugenio carvalho <carvaloalberto03@gmail.com>
2021-03-08 09:33:27 -0300

    tab nav transformado em class

--- js/modules/navtab.js
+++ js/modules/navtab.js
@@ -1,26 +1,39 @@
-export default function initTabMenu() {
-	const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-	const tabcontent = document.querySelectorAll('[data-tab="content"] section');
-
-	function activateTab(index) {
-		tabcontent.forEach((section) => {
-			section.classList.remove('ativo');
-		});
-		tabcontent[index].classList.add('ativo');
+export default class TabNav{
+	constructor(menu, content) {
+		this.tabMenu = document.querySelectorAll(menu);
+		this.tabContent = document.querySelectorAll(content);
+		this.activeClass = 'ativo';
 	}
+	
+	// ativa a tab de acordo com o index da mesma
 
-	if (tabMenu.length && tabcontent.length) {
-		tabcontent[0].classList.add('ativo');
+	activateTab(index) {
+		this.tabContent.forEach((section) => {
+			section.classList.remove(this.activeClass);
+		});
+		this.tabContent[index].classList.add(this.activeClass);
+	}
+	
+	// adiciona os eventos nos tabs
 
-		tabMenu.forEach((itemMenu, index) => {
-		//  nesse caso nao tem como passar a funcao com callback
-		// pois o index se encontar fora do
-		// metodo addeventlister.
-		// lembrando que item menu e o elemento em se
-		// e o index e o numero do item.
+	addTabNavEvent() {
+		this.tabMenu.forEach((itemMenu, index) => {
+			// nesse caso nao tem como passar a funcao com callback
+			// pois o index se encontar fora do
+			// metodo addeventlister.
+			// lembrando que item menu e o elemento em se
+			// e o index e o numero do item.
 			itemMenu.addEventListener('click', () => {
-				activateTab(index);
+				this.activateTab(index);
 			});
 		});
+	}
+
+	init() {
+		if (this.tabMenu.length && this.tabContent.length) {
+			// ativar primeiro item
+			this.activateTab(0);
+			this.addTabNavEvent();
+		}
 	}
 }
